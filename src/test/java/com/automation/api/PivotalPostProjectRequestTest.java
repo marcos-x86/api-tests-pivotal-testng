60b70d73958e5bdc19649f971f5adb2bcdae0c0f
marcos-x86 <marcos.x86@outlook.com>
2021-03-17 21:41:06 -0400

    Post project test implementation

---
+++ src/test/java/com/automation/api/PivotalPostProjectRequestTest.java
@@ -0,0 +1,68 @@
+package com.automation.api;
+
+import io.restassured.RestAssured;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PivotalPostProjectRequestTest {
+
+    String idToDelete = "";
+
+    @Test
+    public void postProjectTest() {
+        // Define headers using map data structure.
+        Map<String, String> headers = new HashMap<>();
+        headers.put("X-TrackerToken", "");
+        headers.put("Content-Type", "application/json");
+
+        // Define request body.
+        String body = "{\"name\": \"AutomationProject 2\"}";
+
+        // Define endpoint.
+        String endpoint = "https://www.pivotaltracker.com/services/v5/projects";
+
+        // Send the POST request and store response in the responseProject variable.
+        Response responseProject = RestAssured.given().headers(headers).body(body).when().post(endpoint);
+
+        // Verifies status code.
+        int responseStatusCode = responseProject.statusCode();
+        int expectedStatusCode = 200;
+        System.out.println("This is the status code of the response: " + responseStatusCode);
+        Assert.assertEquals(responseStatusCode, expectedStatusCode);
+
+        // Verifies that the response body matches with Json Schema.
+        File jsonSchemaFile = new File("src/test/resources/schemas/projectSchema.json");
+        responseProject.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonSchemaFile));
+
+        // Verifies that response body "name" field has the expected value.
+        String responseBody = responseProject.body().asString();
+        System.out.println("This is the body response: " + responseBody);
+        String actualName = responseProject.jsonPath().getString("name");
+        String expectedName = "AutomationProject 2";
+        Assert.assertEquals(actualName, expectedName);
+
+        // Get ID for deleting.
+        idToDelete = responseProject.jsonPath().getString("id");
+    }
+
+    @AfterTest
+    public void deleteProjectPostCondition() {
+        // Define headers using map data structure.
+        Map<String, String> headers = new HashMap<>();
+        headers.put("X-TrackerToken", "");
+        headers.put("Content-Type", "application/json");
+
+        // Define endpoint.
+        String endpoint = "https://www.pivotaltracker.com/services/v5/projects/" + idToDelete;
+
+        // Send delete request.
+        RestAssured.given().headers(headers).when().delete(endpoint);
+    }
+}
