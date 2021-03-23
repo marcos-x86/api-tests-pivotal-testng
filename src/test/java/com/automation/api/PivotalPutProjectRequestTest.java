bc001095cc0b16086889f1ca197ec8970beff9d0
marcos-x86 <marcos.x86@outlook.com>
2021-03-22 20:39:18 -0400

    Put project test added

--- src/test/java/com/automation/api/PivotalPutProjectRequestTest.java
+++ src/test/java/com/automation/api/PivotalPutProjectRequestTest.java
@@ -5,19 +5,21 @@ import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
-public class PivotalPostProjectRequestTest {
+public class PivotalPutProjectRequestTest {
 
-    String idToDelete = "";
+    String endpointBase = "https://www.pivotaltracker.com/services/v5/projects";
     String TOKEN = "";
+    String projectId = "";
 
-    @Test
-    public void postProjectTest() {
+    @BeforeMethod
+    public void createProject() {
         // Define headers using map data structure.
         Map<String, String> headers = new HashMap<>();
         headers.put("X-TrackerToken", TOKEN);
@@ -26,11 +28,34 @@ public class PivotalPostProjectRequestTest {
         // Define request body.
         String body = "{\"name\": \"AutomationProject 2\"}";
 
-        // Define endpoint.
-        String endpoint = "https://www.pivotaltracker.com/services/v5/projects";
-
         // Send the POST request and store response in the responseProject variable.
-        Response responseProject = RestAssured.given().headers(headers).body(body).when().post(endpoint);
+        Response responseProject = RestAssured.given().headers(headers).body(body).when().post(endpointBase);
+
+        // Verifies status code.
+        int responseStatusCode = responseProject.statusCode();
+        int expectedStatusCode = 200;
+        System.out.println("This is the status code of the response: " + responseStatusCode);
+        Assert.assertEquals(responseStatusCode, expectedStatusCode);
+
+        // Extracts ID from created project.
+        projectId = responseProject.jsonPath().getString("id");
+    }
+
+    @Test
+    public void putProjectTest() {
+        // Define headers using map data structure.
+        Map<String, String> headers = new HashMap<>();
+        headers.put("X-TrackerToken", TOKEN);
+        headers.put("Content-Type", "application/json");
+
+        // Define request body.
+        String body = "{\"name\": \"Automation Modified Name\"}";
+
+        // Set endpoint.
+        String putEndpoint = endpointBase + "/" + projectId;
+
+        // Send the PUT request and store response in the responseProject variable.
+        Response responseProject = RestAssured.given().headers(headers).body(body).when().put(putEndpoint);
 
         // Verifies status code.
         int responseStatusCode = responseProject.statusCode();
@@ -39,29 +64,26 @@ public class PivotalPostProjectRequestTest {
         Assert.assertEquals(responseStatusCode, expectedStatusCode);
 
         // Verifies that the response body matches with Json Schema.
-        File jsonSchemaFile = new File("src/test/resources/schemas/projectPOSTSchema.json");
+        File jsonSchemaFile = new File("src/test/resources/schemas/projectPUTSchema.json");
         responseProject.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonSchemaFile));
 
         // Verifies that response body "name" field has the expected value.
         String responseBody = responseProject.body().asString();
         System.out.println("This is the body response: " + responseBody);
         String actualName = responseProject.jsonPath().getString("name");
-        String expectedName = "AutomationProject 2";
+        String expectedName = "Automation Modified Name";
         Assert.assertEquals(actualName, expectedName);
-
-        // Get ID for deleting.
-        idToDelete = responseProject.jsonPath().getString("id");
     }
 
     @AfterMethod
-    public void deleteProjectPostCondition() {
+    public void deleteProject() {
         // Define headers using map data structure.
         Map<String, String> headers = new HashMap<>();
         headers.put("X-TrackerToken", TOKEN);
         headers.put("Content-Type", "application/json");
 
         // Define endpoint.
-        String endpoint = "https://www.pivotaltracker.com/services/v5/projects/" + idToDelete;
+        String endpoint = "https://www.pivotaltracker.com/services/v5/projects/" + projectId;
 
         // Send delete request.
         RestAssured.given().headers(headers).when().delete(endpoint);
